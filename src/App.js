ed42e6adf3aafac46d37280e07c2d784ea567226
redveronika <voronovaveronika@gmail.com>
2017-08-22 14:17:20 +0300

    Added dom testing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Link from "./components/Link";
+import CheckboxWithLabel from "./components/CheckboxWithLabel";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-       <Link />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Link/>
+                <CheckboxWithLabel labelOn="On" labelOff="Off" />
+            </div>
+        );
+    }
 }
 
 export default App;
